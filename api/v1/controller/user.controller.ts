1a5bd0bbf926234519d8e3fb4f565dcf56241fbc
1Huy2409 <nhathuy2409@gmail.com>
2024-12-12 15:02:35 +0700

    detail information

--- api/v1/controller/user.controller.ts
+++ api/v1/controller/user.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import md5 from "md5"
-import User from "../models/user.model"
-import * as generateHelper from "../../helper/generate"
+import md5 from "md5";
+import User from "../models/user.model";
+import * as generateHelper from "../../helper/generate";
 export const register = async (req: Request, res: Response) => {
-    req.body.password = md5(req.body.password);
+  req.body.password = md5(req.body.password);
   //kiem tra email da ton tai chua
   const existEmail = await User.findOne({
     email: req.body.email,
@@ -15,14 +15,12 @@ export const register = async (req: Request, res: Response) => {
       message: "Email already exists",
     });
   } else {
-    const user = new User(
-      {
-        fullName: req.body.fullName,
-        email: req.body.email,
-        password: req.body.password,
-        token: generateHelper.generateRandomString(20)
-      }
-    );
+    const user = new User({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: req.body.password,
+      token: generateHelper.generateRandomString(20),
+    });
     await user.save();
     const token = user.token;
     res.cookie("token", token);
@@ -32,7 +30,57 @@ export const register = async (req: Request, res: Response) => {
       tokenUser: token,
     });
   }
-}
+};
 export const login = async (req: Request, res: Response) => {
-    console.log("ok");
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email does not exist!",
+    });
+    return;
+  }
+  if (md5(password) !== user.password) {
+    res.json({
+      code: 400,
+      message: "Password is incorrect!",
+    });
+    return;
+  }
+  res.cookie("token", user.token);
+  res.json({
+    code: 200,
+    message: "Log in successfully",
+  });
+};
+export const detail = async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+    const user = await User.findOne(
+        {
+            _id: id,
+            deleted: false
+        }
+    )
+    if (user) {
+        res.json(
+            {
+                code: 200, 
+                message: "User be founded!",
+                user: user
+            }
+        )
+    }
+    else {
+        res.json(
+            {
+                code: 400, 
+                message: "User not be founded!"
+            }
+        )
+    }
 }
--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -3,4 +3,6 @@ const router: Router = Router();
 import * as controller from "../controller/user.controller";
 router.post('/register', controller.register);
 router.post('/login', controller.login);
+router.get('/detail/:id', controller.detail);
+
 export const userRoutes: Router = router;
